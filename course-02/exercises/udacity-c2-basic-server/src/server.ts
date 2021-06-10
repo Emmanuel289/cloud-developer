9a0e003e8f7071aea42e3922e34ad92c187eedeb
Emmirald289 <emmirald@gmail.com>
2021-06-10 20:49:46 +0100

    Added description for router controller for root endpoint

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -54,29 +54,105 @@ import { Car, cars as cars_list } from './cars';
   // to demonstrate req.body
   // > try it by posting {"name": "the_name" } as 
   // an application/json body to {{host}}/persons
-  app.post( "/persons", 
-    async ( req: Request, res: Response ) => {
 
-      const { name } = req.body;
+  app.post("/persons", 
+      async (req: Request, res: Response)=>{
 
-      if ( !name ) {
-        return res.status(400)
-                  .send(`name is required`);
-      }
+        const {name}= req.body;
 
-      return res.status(200)
-                .send(`Welcome to the Cloud, ${name}!`);
-  } );
+        
+
+        if (!name){
+          
+          return res.status(400).send(`name is required`);
+        }
+
+        return res.status(200).send(`Welcome to the Cloud, ${name}!`)
+  })
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+
+  app.get("/cars", 
+    async (req: Request, res: Response) => {
+
+      let {make} = req.query;
+
+      let cars_list = cars;
+
+      if (make){
+
+        cars_list = cars.filter((car)=> car.make == make);
+
+
+      }
+
+      return res.status(200).send(cars_list);
+
+      
+
+    });
+
+    
+
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
 
+  app.get("/cars/:id",
+    async (req: Request, res: Response) =>{
+
+      let {id} = req.params;
+
+      if (!id){
+
+        return res.status(400).send(`id is required`);
+      }
+
+      const car = cars.filter((car) => car.id == parseInt(id));
+
+      if (car && car.length === 0){
+
+        return res.status(404).send(`Car not found`);
+      }
+
+      return res.status(200).send(car);
+
+      
+    })
+
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+
+
+  app.post("/cars/", 
+    async (req : Request, res: Response) =>{
+
+      let {id,make, type, model, cost} = req.body;
+
+      // check that all car parameters are entered correctly
+
+      if (!id || !type || !model || !cost){
+
+        return res.status(400).send(`id, type, model, and cost must be entered`);
+      }
+
+      let new_car: Car = { 
+          make: make,
+          type: type, 
+          model: model, 
+          cost: cost, 
+          id: id 
+        };
+
+        cars.push(new_car);
+
+
+        return res.status(201).send(new_car);
+
+      
+    })
 
   // Start the Server
   app.listen( port, () => {
