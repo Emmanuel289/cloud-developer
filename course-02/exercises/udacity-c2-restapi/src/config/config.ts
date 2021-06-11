3f54779414d97fca2661fd4d777a857301693010
Emmirald289 <emmirald@gmail.com>
2021-06-11 08:51:28 +0100

    Added configuration parameters for the database and s3 bucket

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udemmdev",
+    "password": "udemmdev",
+    "database": "udemmdev",
+    "host": "udemmdev.c0x8e19ts5gt.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-emm-dev"
   },
   "prod": {
     "username": "",
